f1969b1f130a44caf308503747273c47c68cbf3c
matomugo60 <matomugo60@gmail.com>
2023-03-21 14:39:14 +0300

    Complete

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCats } from './catsSlice'
+import CatList from '../cats/CatList'
 
 function Cats() {
+  const catPics = useSelector((state) => state.entities)
+  const isLoading = useSelector((state) => state.status)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCats())
+  }, [dispatch])
+
   return (
-    <div>
+    <div className='App'>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      {isLoading === 'loading' && <h1>Loading...</h1>}
+      <CatList catPics={catPics} />
     </div>
-  );
+  )
 }
 
-export default Cats;
+export default Cats
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import catsReducer from './features/cats/catsSlice'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancher = composeWithDevTools(applyMiddleware(thunk))
+const store = createStore(catsReducer, composedEnhancher)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
